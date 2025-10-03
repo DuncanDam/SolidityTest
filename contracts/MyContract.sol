38c6bae8674de4eed6962304ca2432614bb524f0
Duc Anh Dam <damducanh87@gmail.com>
2025-10-03 18:06:22 +0700

    revert change

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -32,7 +32,7 @@ contract MyContract {
         _;
     }
 
-    constructor(string calldata _msg) {
+    constructor(string memory _msg) {
         require(bytes(_msg).length > 0, "Message cannot be empty");
         message = _msg;
         owner = msg.sender;
